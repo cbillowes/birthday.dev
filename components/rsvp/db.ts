739fff9be7483b604057366ca9d750c73621ad04
Clarice Bouwer <cbillowes@gmail.com>
2025-05-26 23:36:41 +0400

    remove lots and alter db saving

--- components/rsvp/db.ts
+++ components/rsvp/db.ts
@@ -4,18 +4,17 @@ import { db } from "@/firebase/config";
 
 export const saveRsvp = async ({ guests, surname }: GuestListType) => {
   const ref = uuidv7();
-  console.log("Saving RSVP for ", surname, " with reference:", ref);
   return guests.map(async (guest) => {
-    console.log("Saving guest:", guest, "with surname:", surname);
     const id = uuidv7();
-    const guestRef = db.collection("guests").doc(id);
-    await guestRef.set({
-      id: uuidv7(),
-      booking: surname,
-      ref,
-      ...guest,
-      createdAt: new Date(),
-    });
-    return guestRef.id;
+    return await db
+      .collection("guests")
+      .doc(id)
+      .set({
+        id: uuidv7(),
+        booking: surname,
+        ref,
+        ...guest,
+        createdAt: new Date(),
+      });
   });
 };
