be792d5cfa3bd004d33604913c37a5b69acaae3f
Clarice Bouwer <cbillowes@gmail.com>
2025-05-26 23:26:33 +0400

    add logs to save rsvp

--- components/rsvp/db.ts
+++ components/rsvp/db.ts
@@ -4,7 +4,9 @@ import { db } from "@/firebase/config";
 
 export const saveRsvp = async ({ guests, surname }: GuestListType) => {
   const ref = uuidv7();
+  console.log("Saving RSVP for ", surname, " with reference:", ref);
   return guests.map(async (guest) => {
+    console.log("Saving guest:", guest, "with surname:", surname);
     const id = uuidv7();
     const guestRef = db.collection("guests").doc(id);
     await guestRef.set({
