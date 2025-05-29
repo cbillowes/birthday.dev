b73424e1f50c9100e22100a8d2c6d4f05593d809
Clarice Bouwer <cbillowes@gmail.com>
2025-05-29 08:11:30 +0400

    configure google analytics

---
+++ lib/firebase.ts
@@ -0,0 +1,26 @@
+import { initializeApp, getApps } from "firebase/app";
+import { getAnalytics, isSupported } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBJMG2DWeECsdFY6Rvu1JIkUN9L-6v1I0Y",
+  authDomain: "skillup-pro.firebaseapp.com",
+  projectId: "skillup-pro",
+  storageBucket: "skillup-pro.firebasestorage.app",
+  messagingSenderId: "572050790053",
+  appId: "1:572050790053:web:0a5d4a52f4fd709dd41f4c",
+  measurementId: "G-7RN1EBPMGY",
+};
+
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
+
+let analytics: ReturnType<typeof getAnalytics> | undefined;
+
+if (typeof window !== "undefined") {
+  isSupported().then((yes) => {
+    if (yes) {
+      analytics = getAnalytics(app);
+    }
+  });
+}
+
+export { app, analytics };
