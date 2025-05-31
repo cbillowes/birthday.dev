717940f842ac3b9b0bf6b3ed7847e8065822bffc
Clarice Bouwer <cbillowes@gmail.com>
2025-05-31 22:36:49 +0400

    trim booking information

--- components/rsvp/db.ts
+++ components/rsvp/db.ts
@@ -6,13 +6,21 @@ export const saveRsvp = async (
   { guests, bookingName }: GuestListType
 ) => {
   const ref = userId;
-  await db.collection("guests").doc(ref).set({
-    userId,
-    bookingName,
-    ref,
-    guests,
-    createdAt: new Date(),
-  });
+  await db
+    .collection("guests")
+    .doc(ref)
+    .set({
+      userId,
+      bookingName: bookingName.trim(),
+      ref,
+      guests: guests.map(({ name, phone, consentForWhatsApp, requests }) => ({
+        name: name.trim(),
+        phone: phone.trim(),
+        consentForWhatsApp,
+        requests: requests ? requests.trim() : "",
+      })),
+      createdAt: new Date(),
+    });
 };
 
 export const getRsvp = async (userId: string) => {
