b0591553c97a85f04bab224ee71ed9410e1cfcf3
Clarice Bouwer <cbillowes@gmail.com>
2025-05-31 22:34:18 +0400

    remove verifyUser

--- components/login/service.ts
+++ components/login/service.ts
@@ -14,24 +14,3 @@ export const loginUser = async ({ email, password }: UserType) => {
   }
   return true;
 };
-
-export const verifyUser = async (token: string) => {
-  try {
-    const response = await fetch("/.netlify/functions/verify", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-    });
-
-    if (!response.ok) {
-      throw new Error("Failed to verify user");
-    }
-
-    return await response.json();
-  } catch (error) {
-    console.error("Error verifying user:", error);
-    throw error;
-  }
-};
